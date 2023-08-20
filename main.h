69f6acf5ce73cda8c74628dc79f7439709e923f6
BereketTeferi <bereketteferi87@gmail.com>
2023-08-20 07:53:52 -0700

    Fixed some bugs

--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+int _printf(const char *format, ...);
 int _putchar(char c);
 int str_print(const char *format, va_list args, int *count, int *array_ctr);
 int char_print(const char *format, va_list args, int *count, int *array_ctr);
