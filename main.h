c44f65ea3505eb3ed9130851f0b7d6695d93a858
BereketTeferi <bereketteferi87@gmail.com>
2023-08-20 05:30:17 -0700

    Added new files

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef "MAIN_H"
+#define "MAIN_H"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+void print_number(int n);
+int _putchar(char c);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * _printf - printf clone
+ *
+ * format: format used for the print
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int count = 0, i = 0;
+	char c, *str;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0' || *format == ' ')
+			{
+				return (-1);
+			}
+			if (*format == 'c')
+			{
+				c = (char)va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				*str = va_arg(args, char*);
+				while(*str)
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				_putchar('%');
+				count++;
+				_putchar(*format);
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
