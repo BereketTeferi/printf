08ce9636dd8373c9c4c1fa4c962f8dcf1f63c277
BereketTeferi <bereketteferi87@gmail.com>
2023-08-20 11:06:44 -0700

    Fixed bugs

--- printf.c
+++ printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 	if (format == NULL)
-		return (count);
+		return (-1);
 	while (format[array_ctr])
 	{
 		if (format[array_ctr] != '%')
