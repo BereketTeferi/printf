c77ea5ce95c41ab26b4812a96b8977c8fd54f8c1
BereketTeferi <bereketteferi87@gmail.com>
2023-08-21 12:06:26 +0000

    Fixed bugs

--- prints.c
+++ prints.c
@@ -23,11 +23,11 @@ int str_print(va_list args, int *count)
 	{
 		while (str[i] != '\0')
 		{
+			_putchar(str[i]);
 			i++;
 			(*count)++;
 		}
-		write(1, str, i);
-		(*count) -= 1;
+		(*count)--;
 	}
 	return (0);
 }
