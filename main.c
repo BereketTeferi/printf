912488b078b4f0298da8c8d83a467fd31e0ade9a
BereketTeferi <bereketteferi87@gmail.com>
2023-08-22 09:31:29 +0000

    FIxed bugs

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	int d = 01234;
+	_printf("%d\n", d);
+	printf("%d\n", d);
+	_printf("%d\n%d\n", INT_MIN, INT_MAX);
+       printf("%d\n%d\n", INT_MIN, INT_MAX);
+	return (0);
+}
