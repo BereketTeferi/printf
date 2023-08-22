2a968420411a4b4c263f6c9628c6f27220f4b466
BereketTeferi <bereketteferi87@gmail.com>
2023-08-22 10:05:45 -0700

    Fixed bugs

--- printf.c
+++ printf.c
@@ -1,6 +1,46 @@
 #include "main.h"
 
 /**
+ * handle_specifier - Handles a single format specifier.
+ *
+ * @specifier: The format specifier character.
+ * @args: va_list of arguments.
+ * @count: Pointer to the count of characters printed.
+ *
+ * Return: 0 on success, -1 on error.
+ */
+int handle_specifier(char specifier, va_list args, int *count)
+{
+	switch (specifier)
+	{
+		case 's':
+			return (str_print(args, count));
+		case 'c':
+			return (char_print(args, count));
+		case 'd':
+		case 'i':
+			return (int_print(args, count));
+		case 'b':
+			return (bin_print(args, count));
+		case '%':
+			return (perc_print(count));
+		case 'u':
+			return (u_print(args, count));
+		case 'o':
+			return (o_print(args, count));
+		case 'x':
+			return (x_print(args, count));
+		case 'X':
+			return (X_print(args, count));
+		default:
+			_putchar('%');
+			_putchar(specifier);
+			(*count) += 2;
+			return (0);
+	}
+}
+
+/**
  * _printf - Custom printf function.
  * @format: Format string containing directives.
  *
@@ -26,31 +66,8 @@ int _printf(const char *format, ...)
 			array_ctr++;
 			if (format[array_ctr] == ' ' || format[array_ctr] == '\0')
 				return (-1);
-			if (format[array_ctr] == 's')
-				str_print(args, &count);
-			else if (format[array_ctr] == 'c')
-				char_print(args, &count);
-			else if (format[array_ctr] == 'd' || format[array_ctr] == 'i')
-				int_print(args, &count);
-			else if (format[array_ctr] == 'b')
-				bin_print(args, &count);
-			else if (format[array_ctr] == '%')
-				perc_print(&count);
-			else if (format[array_ctr] == 'u')
-				u_print(args, &count);
-			else if (format[array_ctr] == 'o')
-				o_print(args, &count);
-			else if (format[array_ctr] == 'x')
-				x_print(args, &count);
-			else if (format[array_ctr] == 'X')
-				X_print(args, &count);
-			else
-			{
-				_putchar(format[array_ctr - 1]);
-				count++;
-				_putchar(format[array_ctr]);
-				count++;
-			}
+			if (handle_specifier(format[array_ctr], args, &count) == -1)
+				return (-1);
 		}
 		array_ctr++;
 	}
